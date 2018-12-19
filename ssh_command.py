74e7719cb44e53e2eef2df61ef977b02a7c8ac8b
Jason Casas <44911805+casajaso@users.noreply.github.com>
2018-12-18 17:03:49 -0800

    updated ssh_command.py
    refactored argument parsing and added logic for standard password authentication.

--- ssh_command.py
+++ ssh_command.py
@@ -9,44 +9,50 @@ import paramiko
 import getpass
 
 def get_args(argv): #gets vars passed via cli
-    parser = argparse.ArgumentParser(description='runs commands on remote host over ssh')
-    parser.add_argument('-u', '--user', nargs='?', dest='username', help=' <username>')
-    parser.add_argument('-n', '--name', nargs='?', dest='hostname', help=' <hostname>')
-    parser.add_argument('-p', '--proxy', nargs='?', default=None, dest='proxy', help=' <(OPTIONAL)proxy>')
-    parser.add_argument('-i', '--id-file', nargs='?', default=None, dest='identityfile', help=' <(OPTIONAL)identity file(PEM)>')
-    parser.add_argument('-c', '--command', nargs='?', dest='command', help=' <\'";" seperated command(s)\'>')
+    parser = argparse.ArgumentParser(description='run commands on remote host over ssh')
+    parser.add_argument('hostname', nargs='?', help=' target hostname or ip (***required)')
+    parser.add_argument('-c', '--command', required=True, nargs='+', help=' command(s) to execute on target host (***required) example: -c \'command-1\' OR -c \'command-2; command-3\'')
+    parser.add_argument('-b', '--bastian', nargs='?', default=None, help=' proxy through a bastion/jumpbox (default: %(default)s)')
+    parser.add_argument('-u', '--username', nargs='?', default=(getpass.getuser()), help=' username on target host (default: %(default)s)')
+    parser.add_argument('-i', '--identityfile', nargs='?', default=None, help=' use ssh identify file; opens prompt (default: %(default)s)')
+    parser.add_argument('-p', '--password', action='store_true', help=' use password; opens prompt (default: no flag set)')
     return parser.parse_args()
 
-def get_opts(identityfile, proxy): #sets logic around optional vars 
-    if (proxy != None):
-        proxy_command = 'ssh -qxTA {} nc {} {}'.format(proxy, hostname, '22')
-        proxy_socket = paramiko.proxy.ProxyCommand(proxy_command)
+def get_opts(password, username, identityfile, bastian, hostname): #sets logic around optional vars
+    if (password):
+        password = getpass.getpass(prompt='input password for {}: '.format(username))
     else:
-        proxy_socket = proxy
+        password = None
     if (identityfile != None):
-        passphrase = getpass.getpass(prompt='passphrase for {}: '.format(identityfile))
+        passphrase = getpass.getpass(prompt='input passphrase for {}: '.format(identityfile))
         keyfile = paramiko.RSAKey.from_private_key_file(identityfile, password=passphrase)
     else:
         keyfile = identityfile
-    return {'proxy':proxy_socket, 'keyfile':keyfile}
+    if (bastian != None):
+        proxy_command = 'ssh -qxTA {} nc {} {}'.format(bastian, hostname, '22')
+        proxy_socket = paramiko.proxy.ProxyCommand(proxy_command)
+    else:
+        proxy_socket = bastian
+    return {'proxy':proxy_socket, 'keyfile':keyfile, 'password':password}
 
-def run_ssh(hostname, username, command, keyfile, proxy): #creates ssh session and runs command
+def run_ssh(hostname, username, password, keyfile, proxy, commands): #creates ssh session and runs command
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        ssh_client.connect(hostname=hostname, username=username, timeout=None, allow_agent=True, look_for_keys=True, pkey=keyfile, sock=proxy)
-        stdin, stdout, stderr = ssh_client.exec_command(command) 
-        if stdout.channel.recv_exit_status() != 0:
-            print '[ERROR][exec_command]: {}'.format(stderr.read())
-        print stdout.read()
+        ssh_client.connect(hostname=hostname, username=username, password=password, timeout=None, allow_agent=True, look_for_keys=True, pkey=keyfile, sock=proxy)
+        for command in commands:
+            stdin, stdout, stderr = ssh_client.exec_command(command)
+            if stdout.channel.recv_exit_status() != 0:
+                print '[***ERROR][REMOTE][EXEC]: {}'.format(stderr.read())
+            print stdout.read()
         ssh_client.close()
     except Exception as err:
-        print '[ERROR][ssh_client.connect]: {}'.format(err)
+        print '[***ERROR][LOCAL][CONNECT]: {}'.format(err)
 
 def main(argv):
     args = get_args(argv)
-    opts = get_opts(args.identityfile, args.proxy)
-    run_ssh(args.hostname, args.username, args.command, opts['keyfile'], opts['proxy'])
+    opts = get_opts(args.password, args.username, args.identityfile, args.bastian, args.hostname)
+    run_ssh(args.hostname, args.username, opts['password'], opts['keyfile'], opts['proxy'], args.command)
         
 if __name__ == '__main__': 
-    main(sys.argv[1:])
+    main(' '.join(sys.argv[1:]))
